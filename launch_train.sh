875271cfef6b5de1965d0dce1821f3ec0060a053
Elena Fillola <ef17148@bristol.ac.uk>
2025-07-31 12:03:24 +0100

    updated launch file

--- launch_train.sh
+++ launch_train.sh
@@ -2,20 +2,22 @@
 #SBATCH --partition=gpu
 #SBATCH --mem=300GB
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=modloss
-#SBTACH --output=modloss
+#SBATCH --job-name=train
+#SBTACH --output=train
 #SBATCH --time=24:00:00
-#SBATCH --account=chem007981
+#SBATCH --account=SEMT030444
 #SBATCH --exclude=bp1-gpu030,bp1-gpu035
 #### this is specific to the University of Bristol's BluePebble
 #### make sure you modify to remove/add any relevant modules
+
+## chem007981  SEMT030444 
 
 # the two gpus above seem to have a different version of cuda! avoid
 
 echo "activate env"
 # activate your own environment here
 conda init
-conda activate new_graphnet
+conda activate graphnet
 
 
 echo "loading modules"
@@ -25,4 +27,4 @@ module add languages/python/3.12.9.tensorflow-2.16.1
 
 
 echo "train"
-python general_train.py parameter_template.txt --file_path /user/work/ef17148/GCN/graphnet/repo_test/graphnet_LPDM_emulator/
+python general_train.py parameter_file_paper.json --file_path /graphnet_LPDM_emulator/parameter_files/
