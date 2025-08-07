8f8186ace5c7aa27ce2bc7dfc86f9a8bffbc8539
Jeff Clark <jeff127@gmail.com>
2025-08-07 18:12:07 +0100

    Integrate uncertainty_calculate into uncertainty_fluxes.ipynb

--- model/forecast.py
+++ model/forecast.py
@@ -12,9 +12,9 @@ import torch
 from typing import Optional
 
 
-from model.layers.encoder import SatelliteEncoder, FixedSatelliteEncoder
-from model.layers.decoder import SatelliteDecoder
-from model.layers.processor import SatelliteProcessor
+from graphnet_LPDM_emulator.model.layers.encoder import SatelliteEncoder, FixedSatelliteEncoder
+from graphnet_LPDM_emulator.model.layers.decoder import SatelliteDecoder
+from graphnet_LPDM_emulator.model.layers.processor import SatelliteProcessor
 import numpy as np
 
 
