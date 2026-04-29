953dc908e57e1d4750c6082d396dbed6c24b05a6
Elena Fillola <ef17148@bristol.ac.uk>
2026-04-29 18:28:59 +0100

    added plotting functions with notebook

---
+++ gates/evaluation/post_processing.py
@@ -0,0 +1,12 @@
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+def threshold_fps(fps, threshold, fp_label=["fp_pred"]):
+    """
+    Takes in an xarray and for each variable in fp_labels, applies the given threshold to set values below the threshold to zero. It saves it as a new variable in the xarray with the name "{original_variable_name}_thres".
+    """
+    for label in fp_label:
+        thres_label = f"{label}_thres"
+        fps[thres_label] = fps[label].where(fps[label] >= threshold, 0)
+    return fps
---
+++ gates/plotting/__init__.py
@@ -0,0 +1,5 @@
+from .plotting_predictions import plot_fp_predictions
+
+__all__ = [
+    "plot_fp_predictions",
+]
